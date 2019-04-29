7fdf5d731e57bba59a1bdc0e6e36e042cf3ae4a7
cyniborg <cyniborg@gmail.com>
2019-04-29 11:07:07 +0200

    Included nuka-carousel from npm to display the options as a slider carousel. Added select and hint display event handlers.

--- src/components/BodyContent/Gameplay.js
+++ src/components/BodyContent/Gameplay.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Carousel from 'nuka-carousel';
+import colours from '../../resources/colours';
+import Hint from './Hint';
+
 
 
 const Question = (props)=>(
@@ -17,54 +20,84 @@ Question.propTypes = {
 };
 
 const Options = (props) => (
-  <div className= {`option-${props.index}`}>
-      <div className="option-header">
-          <h4>OPTION</h4>
-          <p>{props.index}<span>/{props.total}</span></p>
-      </div>
-      <div className="option-body">
-          <div className="option-content">
-            {props.optionText}
-          </div>
-          <div className="submit-button">
-            <button>Select</button>
-          </div>
-      </div>
-  </div>
-);
-
-Options.propTypes = {
-  index: PropTypes.number.isRequired,
-  total: PropTypes.number,
-  optionText: PropTypes.string
-}
+    <div className= {`opt-${props.index}`}>
+        <div className="option-header" style = {{backgroundColor: props.colour}}>
+            <h4>OPTION</h4>
+            <p>{props.index+1}<span>/{props.total}</span></p>
+        </div>
+            <div className="option-content">
+              {props.optionText}
+            </div>
+            <div className="submit-button">
+              <button onClick = {(e)=>{props.handleClick(e, false);}}>Select</button>
+            </div>
+    </div>
+  );
+  
+  Options.propTypes = {
+    index: PropTypes.number.isRequired,
+    total: PropTypes.number,
+    optionText: PropTypes.string,
+    colour: PropTypes.string
+  }
 
 class GameplayBody extends Component{
     constructor(props){
         super(props);
         this.state = {
-            totalQuestions: 0,
+            totalQuestions: this.props.data.length,
             currentQuestionNumber: 0,
-            data: {}
+            data: this.props.data,
+            showHint: false
         }
+    }
+
+    handleClick = (event, hint = false)=>{
+        event.preventDefault();
+        this.setState((prev,next)=>({
+            showHint: !prev.showHint,
+            currentQuestionNumber: hint && prev.currentQuestionNumber<this.state.totalQuestions-1?prev.currentQuestionNumber + 1:prev.currentQuestionNumber
+        }))
     }
 
     render(){
         return(
+            <React.Fragment>
+            { this.state.data===null ? null : 
+            <div className="body-gameplay" style = {{backgroundColor:this.props.genreData.colour}}>
+                <Question currentQuestionNumber = {this.state.currentQuestionNumber+1} totalQuestions = {this.state.totalQuestions} currentQuestion = { this.props.data[this.state.currentQuestionNumber].questionText} />
+                
+                <div className="options">
+                    <div className="options-container">
+                        <Carousel
+                        slideWidth = {0.24}
+                        cellSpacing = {20}
+                        renderBottomCenterControls = {null}
+                        heightMode = 'max'
+                         >
+                        {this.state.data[this.state.currentQuestionNumber].options.map((e,i)=>(
+                            <Options key = {i} index = {i} colour = {i>=colours.options.length?colours.options[i%colours.options.length]:colours.options[i]} total = {this.state.data[this.state.currentQuestionNumber].options.length} optionText = {e} handleClick = {this.handleClick} />
+                        ))}
+                        </Carousel>
+                    </div>
+                </div>
             
-    <div className="body-gameplay">
-        <Question currentQuestionNumber = {this.state.currentQuestionNumber} totalQuestions = {this.state.totalQuestions} currentQuestion = { this.state.data[this.state.currentQuestionNumber].question} />
+                <div className="img-gameplay"><img className = "img-responsive" src={this.props.genreData.image} alt="image" /></div>
+            </div>
+            }
+            {this.state.showHint && <Hint hint = {this.state.data[this.state.currentQuestionNumber].hint} handleClick = {this.handleClick} />}
+            </React.Fragment>
+        )
+    }
+}
+
+export default GameplayBody;
+
+/*<Question currentQuestionNumber = {this.state.currentQuestionNumber} totalQuestions = {this.state.totalQuestions} currentQuestion = { this.state.data[this.state.currentQuestionNumber].question} />
         <div className="options">
             <div className="options-container">
                 <Carousel>
                   <Options />
                 </Carousel>
             </div>
-        </div>
-        <div className="img-gameplay"><img src="" alt="" /></div>
-    </div>
-        )
-    }
-}
-
-export default GameplayBody;
+        </div>*/
