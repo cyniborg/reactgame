6b5526c182a97d18e958c73a30f0c0a6efc8678f
cyniborg <cyniborg@gmail.com>
2019-04-29 10:48:01 +0200

    Included image and colour section for REST response for genres. This sets the colour and associated image directly from the backend rather than the frontend

--- src/axios/getData.js
+++ src/axios/getData.js
@@ -7,10 +7,30 @@ function getGenre(){
         url:process.env.REACT_APP_GENRE_URL
     })
     .then(function (response) {
-        const genre = response.data.map((e)=>({id: e.id, name: e.name}));
+        const genre = response.data.map((e)=>({id: e.id, name: e.name, "genreColour": e.genre_colour, "genreImage": e.genre_image.guid}));
         console.log(genre);
         return genre;
     });
 }
 
-export default getGenre;
+function getQuestions(){
+    return axios({
+        method: 'get',
+        url: `${process.env.REACT_APP_QN_URL}`
+    })
+    .then(function(response){
+        const data = response.data.map((e)=>{
+            let options = [];
+            for(let i = 1; i<7; i++){
+                if(e[`option_${i}`]!==""){
+                    options.push(e[`option_${i}`]);
+                }
+            }
+            return {"questionText":e.question,"options": options, "genreId": e.genre, "hint": e.hint};
+        })
+        console.log(data);
+        return data;
+    });
+}
+
+export {getGenre, getQuestions};
