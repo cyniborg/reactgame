7fc4985d38d5af37b146f95321cded6731d4c1e9
cyniborg <cyniborg@gmail.com>
2019-04-29 11:08:25 +0200

    Removed the base colour option as it is not required anymore. Nw only the options head colour and the accent colour is selected from here.

--- src/resources/colours.js
+++ src/resources/colours.js
@@ -1,5 +1,4 @@
 const colours = {
-    "base": ["yellow", "purple", "pink", "beige", "orange"],
     "options": ["#fc6700", "#d12d28", "#008fc4", "#008d84"],
     "accent" : ["#d19428", "#5a5ae5", "#cc71d1", "#e05a5a", "#ed8129"]    
 };
