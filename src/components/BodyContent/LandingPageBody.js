7ba05effa1c10c93ac71dae2c976229a259241b4
cyniborg <cyniborg@gmail.com>
2019-04-29 11:04:32 +0200

    The colours are now directly retrievd from the genre detail rather than setting explicitly in the code.

--- src/components/BodyContent/LandingPageBody.js
+++ src/components/BodyContent/LandingPageBody.js
@@ -1,17 +1,25 @@
 import React, {Component} from 'react';
-import getGenre from '../../axios/getData';
-import colours from '../../resources/colours'
+import {getGenre} from '../../axios/getData';
+import PropTypes from 'prop-types';
 
 const LandingPageSection = (props)=>(
-    <div className={`section-${props.count} ${props.colour}`} onClick = {()=>{props.handleChange(false, false, props.id)}}>
+    <div className={`section-${props.count}`} style = {{backgroundColor:props.colour}} onClick = {()=>{props.handleChange(false, false, {'id': props.id, 'colour': props.colour, 'image': props.img})}}>
         <div className="landing-text">
         {props.name}
         </div>
         <div className="landing-box-img">
-        <img className="img-responsive" src="img/market-analyse-landing.png" alt="People discussing market analyses" />
+        <img className="img-responsive" src={props.img} alt={props.name + "image"} />
         </div>
     </div>
 );
+
+LandingPageSection.propTypes = {
+    count: PropTypes.number.isRequired,
+    colour: PropTypes.string,
+    handleChange: PropTypes.func,
+    img: PropTypes.string,
+    name: PropTypes.string.isRequired
+}
 
 class LandingPageBody extends Component{ 
     constructor(props){
@@ -34,14 +42,7 @@ class LandingPageBody extends Component{
         return(
             <div className="body-landing">
                 {this.state.genre.length>0 && this.state.genre.map((e,i)=>{
-                    const col = colours.base.length;
-                    let colour;
-                    if(i<col){
-                        colour = colours.base[i];
-                    } else if(i>col){
-                        colour = colours.base[i%col];
-                    }
-                    return <LandingPageSection key = {e.id} count={i} name = {e.name} colour = {colour} id = {e.id} handleChange = {this.props.handleChange} />;
+                    return <LandingPageSection key = {e.id} count={i} name = {e.name} colour = {e.genreColour} img = {e.genreImage} id = {e.id} handleChange = {this.props.handleChange} />;
                     })}
             </div>
         )
