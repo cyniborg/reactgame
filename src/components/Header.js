8ace24791d23a567f5b3c56729121f5d1e919e99
cyniborg <cyniborg@gmail.com>
2019-05-13 10:38:34 +0200

    Changes in the display styling. And kept the score screen display on gameover

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,8 @@ function GameplayHeader(props){
     const style = {backgroundColor: props.genre.colour};
     return(
         <React.Fragment>
-            <div className="level" style = {style}>Level<br /><span>{props.genre.name}</span></div>
-            <div className="score" style = {style}>Score<br /><span>{props.score}</span></div>
-            <div className="Hhint" style = {style}>Hint</div>
+            <div className="level" style = {style}><h4>Level<br /><span>{props.genre.name}</span></h4></div>
+            <div className="score" style = {style}><h4>Score<br /><span>{props.score}</span></h4></div>
         </React.Fragment>
     );
 }
@@ -23,9 +22,11 @@ class Header extends Component {
     render(){
         return(
             <div className="header">
-            <div className="logo"> This will be an image</div>
+            <div className="logo">
+                <img className= "img-responsive" src="./img/shedidit-logo.png" alt="SheDidIt Logo"/>
+            </div>
             <div className="dashboard game-dash">
-                {!this.props.landingPage && this.props.genre!==null ?  <GameplayHeader score = {this.props.score} genre = {this.props.genre} />:<h1>Welcome To The Game</h1> }
+                {!this.props.landingPage && this.props.genre!==null ?  <GameplayHeader score = {this.props.score} genre = {this.props.genre} />: <h1>Welcome To The Game</h1> }
             </div>
         </div>
         )
