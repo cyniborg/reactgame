8c5a1e2fb9c346a20b697f3a5e7af84f8d74e3eb
cyniborg <cyniborg@gmail.com>
2019-05-13 10:34:09 +0200

    Changes due to more incoming data in the GET call.

--- src/components/BodyContent/Gameplay.js
+++ src/components/BodyContent/Gameplay.js
@@ -3,13 +3,15 @@ import PropTypes from 'prop-types';
 import Carousel from 'nuka-carousel';
 import colours from '../../resources/colours';
 import Hint from './Hint';
+import GameOver from './GameOver';
+
 import { CSSTransition } from 'react-transition-group';
 
 
 
 const Question = (props)=>(
     <div className="question">
-            <h4>Question {props.currentQuestionNumber}/{props.totalQuestions}</h4>
+            <p>Question {props.currentQuestionNumber}/{props.totalQuestions}</p>
             <p>{props.currentQuestion}</p>
         </div>
 );
@@ -25,17 +27,17 @@ const Options = (props) => (
 
     <div className= {`opt-${props.index} flipInY`}>
         <div className="option-header" style = {{backgroundColor: props.colour}}>
-            <h4>OPTION</h4>
-            <p>{props.index+1}<span>/{props.total}</span></p>
+            <p>OPTION</p>
+            <p className= "option-number">{props.index+1}<span>/{props.total}</span></p>
         </div>
             <div className="option-content">
-              {props.optionText}
+              {props.option.text}
             </div>
             <div className="submit-button">
-              <button className="option-select" onClick = {(e)=>{
+              <button style = {{backgroundColor: props.btnCol}} className="option-select" onClick = {(e)=>{
                   props.handleClick(e, false);
-                  props.changeScore(5, false);
-                  }}>
+                  props.changeScore(props.option.score, false);
+                  }} >
                   Select
                   </button>
             </div>
@@ -46,8 +48,8 @@ const Options = (props) => (
   Options.propTypes = {
     index: PropTypes.number.isRequired,
     total: PropTypes.number,
-    optionText: PropTypes.string,
-    colour: PropTypes.string
+    option: PropTypes.object,
+    colour: PropTypes.string,
   }
 
 class GameplayBody extends Component{
@@ -58,7 +60,8 @@ class GameplayBody extends Component{
             currentQuestionNumber: 0,
             data: this.props.data,
             showHint: false,
-            inProp: true
+            inProp: true,
+            windowWidth: 1199 
         }
     }
 
@@ -82,15 +85,27 @@ class GameplayBody extends Component{
         }
     }
 
+    
+    checkWidth = ()=>{
+        this.setState({windowWidth: window.innerWidth});
+        this.forceUpdate();
+    }
+    
+    componentDidMount(){
+        this.checkWidth();
+    }
+    
     render(){
+        window.onresize = this.checkWidth;
         return(
+            <div className="body-gameplay slideInRight" style = {!this.props.gameOver?{backgroundColor:this.props.genreData.colour}:null}>
+            { this.state.data===null || this.props.gameOver === true ? null : 
             <React.Fragment>
-            { this.state.data===null ? null : 
-            <div className="body-gameplay" style = {{backgroundColor:this.props.genreData.colour}}>
                 <CSSTransition
                 in = {this.state.inProp}
                 timeout = {300}
-                classNames = "opt-anim"
+                classNames = "zoomIn"
+                mountOnEnter
                 unmountOnExit
                 >
                 <Question currentQuestionNumber = {this.state.currentQuestionNumber+1} totalQuestions = {this.state.totalQuestions} currentQuestion = { this.props.data[this.state.currentQuestionNumber].questionText} />
@@ -99,37 +114,41 @@ class GameplayBody extends Component{
                 <div className="options">
                     <div className="options-container">
                         <Carousel
-                        slideWidth = {0.24}
+                        slideWidth = {this.state.windowWidth>1199 ? 0.24 : this.state.windowWidth<600 ? 0.95 : 0.45}
                         cellSpacing = {20}
                         renderBottomCenterControls = {null}
                         heightMode = 'max'
+                        enableKeyboardControls = {true}
+                        renderCenterLeftControls = {null}
+                        renderCenterRightControls = {null}
                          >
                         {this.state.data[this.state.currentQuestionNumber].options.map((e,i)=>(
                             <CSSTransition
                             key = {i}
                             in = {this.state.inProp}
-                            timeout = {300}
+                            timeout = {600}
                             classNames = "opt-anim"
                             unmountOnExit
                             >
 
-                            <Options key = {i} index = {i} colour = {i>=colours.options.length?colours.options[i%colours.options.length]:colours.options[i]} total = {this.state.data[this.state.currentQuestionNumber].options.length} optionText = {e} handleClick = {this.handleClick} changeScore = {this.props.changeScore} />
+                            <Options key = {i} index = {i} colour = {i>=colours.options.length?colours.options[i%colours.options.length]:colours.options[i]} total = {this.state.data[this.state.currentQuestionNumber].options.length} option = {e} handleClick = {this.handleClick} changeScore = {this.props.changeScore} btnCol = {this.props.genreData.colour} />
                             
                             </CSSTransition>
                         ))}
                         </Carousel>
                     </div>
-                </div>
+                </div>,
             
-                <div className="img-gameplay"><img className = "img-responsive" src={this.props.genreData.image} alt="image" /></div>
-            </div>
+                <div className="img-gameplay"><img className = "img-responsive" src={this.props.genreData.image} alt={this.props.genreData.name} /></div>
+            </React.Fragment>
             }
             
 
-            {this.state.showHint && <Hint showHint = {this.state.showHint} hint = {this.state.data[this.state.currentQuestionNumber].hint} handleClick = {this.handleClick} />
+            {this.state.showHint ? <Hint showHint = {this.state.showHint} hint = {this.state.data[this.state.currentQuestionNumber].hint} handleClick = {this.handleClick} /> : null
             }
+            {this.props.gameOver && !this.state.showHint ? <GameOver gameOver = {this.props.gameOver} resetGame = {this.props.resetGame} />:null}
+            </div>
             
-            </React.Fragment>
         )
     }
 }
