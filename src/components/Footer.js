138bac10897082282f1e4fbd009b3cf39d942ddb
cyniborg <cyniborg@gmail.com>
2019-05-13 10:37:51 +0200

    Changes in the display styling. Added the icons and contact us

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,30 @@
 import React from 'react';
 
+const SocialIcons = (props) => (
+    <React.Fragment>
+        <div className="text">
+            <h5>Lets socialize:&nbsp;</h5>
+        </div>
+        <div className="icon-1">
+            <img src="./img/facebook.svg" alt="facebook"/>
+        </div>
+        <div className="icon-2">
+            <img src="./img/twitter.svg" alt="twitter"/>
+        </div>
+        <div className="icon-3">
+            <img src="./img/youtube.svg" alt="youtube"/>
+        </div>
+        <div className="icon-4">
+            <img src="./img/linkedin.svg" alt="linkedin"/>
+        </div>
+    </React.Fragment>
+)
+
 const Footer = (props)=>(
     <div className="footer">
-        <div className="social">Social icone</div>
-        <div className="copyright">Copyright</div>
-        <div className="contact">Contact</div>
+        <div className="social">{<SocialIcons />}</div>
+        <div className="copyright">&copy; 2019 Some Rights Reserved by #SheDidIt </div>
+        <div className="contact"><a href="https://www.shedidit.be/">Contact Us</a></div>
     </div>
 );
 
