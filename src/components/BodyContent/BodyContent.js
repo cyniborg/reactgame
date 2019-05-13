78bcb0ed221e23c5c33c99a163ca9d7554f46411
cyniborg <cyniborg@gmail.com>
2019-05-13 10:29:30 +0200

    passing props for determining game over state and reset game state

--- src/components/BodyContent/BodyContent.js
+++ src/components/BodyContent/BodyContent.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import LandingPageBody from './LandingPageBody';
 import GameplayBody from './Gameplay';
-import GameOver from './../GameOver';
-//import {getQuestions} from '../../axios/getData';
 
 class BodyContent extends Component{
 
@@ -10,9 +8,14 @@ class BodyContent extends Component{
     render(){
         return(
             <React.Fragment>
-                {this.props.data.landingPage && this.props.data.data!==null ? <LandingPageBody changeScore = {this.props.changeScore} handleChange = {this.props.handleChange} />: null}
-                {this.props.data.gameplayData!==null && !this.props.data.landingPage && !this.props.data.gameOver ? <GameplayBody data = {this.props.data.gameplayData} genreData = {this.props.data.genreData} handleChange = {this.props.handleChange} changeScore = {this.props.changeScore} />: null}
-                {this.props.data.gameOver && <GameOver gameOver = {this.props.data.gameOver} />}
+                {
+                    this.props.data.landingPage && this.props.data.data!==null ? <LandingPageBody changeScore = {this.props.changeScore} handleChange = {this.props.handleChange} genre = {this.props.genre} /> : null
+                }
+
+                {
+                    this.props.data.gameplayData!==null && !this.props.data.landingPage ? <GameplayBody data = {this.props.data.gameplayData} genreData = {this.props.data.genreData} handleChange = {this.props.handleChange} changeScore = {this.props.changeScore} gameOver = {this.props.data.gameOver} resetGame = {this.props.resetGame} /> : null
+                }
+                
             </React.Fragment>
         )
     }
